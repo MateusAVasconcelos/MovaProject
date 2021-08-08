9781a67b6288988177eb84d4878988f03bfe056f
Mateus Vasconcelos <mateus1030@gmail.com>
2021-08-08 16:35:43 -0300

    add index.html

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-app>
+    <v-app-bar
+      app
+      color="white"
+      dark
+    >
+      <div class="d-flex align-center">
+        <v-img
+          alt="Mova Logo"
+          class="shrink mr-2"
+          contain
+          src="https://mova.vc/img/footer_mova.svg"
+          transition="scale-transition"
+          width="100"
+        />
+
+        <v-icon
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          width="100"
+        >
+        </v-icon>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
