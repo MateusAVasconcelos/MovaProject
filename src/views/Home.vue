236670cd4b8dad19a9b96d30b6299ddc139ea349
Mateus Vasconcelos <mateus1030@gmail.com>
2021-08-23 17:28:59 -0300

    feat: alguns filtros feitos, lingua ainda em bug

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,35 @@
 <template>
 <div>
   <v-container>
-    <!-- <v-row>
-      <v-flex xs12 sm3>
-        <v-select
-          :items="items"
+    <v-row class="pl-1 pt-7 pb-7">
+      <v-flex xs12 sm4>
+        <v-autocomplete
+          v-model="filtered"
+          :items="Filtros"
           label="Escolha uma opção"
-          single-line
+          item-text="nome"
+          item-value="value"
+          @change="filter($event)"
           hide-details
+          clearable
+          style="width: 80%"
           >
-        </v-select>
+        </v-autocomplete>
       </v-flex>
-      <v-flex xs12 sm3>
+      <v-flex xs12 sm4>
+        <v-autocomplete
+          v-if="filtered"
+          :items="filterList"
+          item-text = "nome"
+          item-value = "nome"
+          label= "temporario"
+          style="width: 80%"
+          @change="getFlags($event)"
+          hide-details
+        >
+        </v-autocomplete>
+      </v-flex>
+      <v-flex xs12 sm4>
         <v-btn
           color="#6D2080"
           dark
@@ -19,12 +37,13 @@
           Pesquisar
         </v-btn>
       </v-flex>
-    </v-row> -->
+    </v-row>
     <v-row>
       <v-flex v-for="flag in FlagList" :key="flag.index" sm4 xs12>
         <img width="80%" height="80%" :src="flag.flag"/>
       </v-flex>
     </v-row>
+    
   </v-container>
 </div>
 </template>
@@ -37,10 +56,33 @@
       // ArticleItem,
     },
     data: () => ({
+      Filtros: [
+        {
+          nome: "Região",
+          value: "1"
+        },
+        {
+          nome: "Capital",
+          value: "2"
+        },
+        {
+          nome: "Língua",
+          value: "3"
+        },
+        {
+          nome: "País",
+          value: "4"
+        },
+        {
+          nome: "Código de ligação",
+          value: "5"
+        },
+      ],
+      linguas: [],
+      filtered: null,
       article: [],
-      currentPage: 1,
-      pageCount: 0,
       FlagList: [],
+      filterList:[],
     }),
     methods:{
 
@@ -50,8 +92,57 @@
         .then((res) => {
           this.FlagList = res.data;
         })
-        console.log(this.FlagList)
+        // this.FlagSlice = this.FlagList.slice(0, 12);
       },
+
+      filter(){
+        switch (this.filtered) {
+          case "1":
+            this.axios.get('https://restcountries.eu/rest/v2/all?fields=region')
+            .then((res)=> {
+              this.filterList = res.data.filter(element => {
+                element.nome = element.region
+                return element;
+              });
+            })
+            break;
+          case "2":
+            this.axios.get('https://restcountries.eu/rest/v2/all?fields=capital')
+            .then((res)=>{
+              this.filterList = res.data.filter(element => {
+                element.nome = element.capital
+                return element;
+              });
+            })
+            break;
+          case "3":
+            this.axios.get('https://restcountries.eu/rest/v2/all?fields=languages')
+            .then((res)=>{
+              this.linguas = res.data;
+            })
+            this.filterList = this.linguas.map((lingua)=>{
+              for(var i=0;i<lingua.languages.lenght;i++){
+                console.log(lingua.languages.lenght)
+                if(lingua){
+                  return {
+                    id: lingua.languages[i].iso639_1,
+                    nome: lingua.languages.nativeName
+                  }
+                }
+              }
+            })
+            console.log(this.filterList.length)
+            break;
+          default:
+            this.axios.get('https://restcountries.eu/rest/v2/all?fields=name')
+            .then((res)=>{
+              this.filterList = res.data.filter(element => {
+                element.nome = element.name
+                return element;
+              })
+            })
+        }
+      }
     },
     mounted(){
       this.getFlags()
