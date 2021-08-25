26687807ecf3d69d3723ccf061151a5dcefbdc5b
Mateus Vasconcelos <mateus1030@gmail.com>
2021-08-24 23:58:29 -0300

    filtros funcionando

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,174 +1,195 @@
 <template>
 <div>
-  <v-container>
-    <v-row class="pl-1 pt-7 pb-7">
-      <v-flex xs12 sm4>
-        <v-autocomplete
-          v-model="filtered"
-          :items="Filtros"
-          label="Escolha uma opção"
-          item-text="nome"
-          item-value="value"
-          @change="filter($event)"
-          hide-details
-          clearable
-          style="width: 80%"
-          >
-        </v-autocomplete>
-      </v-flex>
-      <v-flex xs12 sm4>
-        <v-autocomplete
-          v-if="filtered"
-          :items="filterList"
-          item-text = "nome"
-          item-value = "nome"
-          label= "temporario"
-          style="width: 80%"
-          @change="getFlags($event)"
-          hide-details
-        >
-        </v-autocomplete>
-      </v-flex>
-      <v-flex xs12 sm4>
-        <v-btn
-          color="#6D2080"
-          dark
-        >
-          Pesquisar
-        </v-btn>
-      </v-flex>
-    </v-row>
-    <v-row>
-      <v-flex v-for="flag in FlagList" :key="flag.index" sm4 xs12>
-        <img width="80%" height="80%" :src="flag.flag"/>
-      </v-flex>
-    </v-row>
-    
-  </v-container>
-</div>
+		<v-container>
+			<v-row class="pl-1 pt-7 pb-7">
+				<v-flex xs12 sm4>	
+					<v-autocomplete
+						v-model="filtered"
+						:items="Filtros"
+						label="Escolha uma opção"
+						item-text="nome"
+						item-value="value"
+						@change="filter($event)"
+						hide-details
+						clearable
+						style="width: 80%"
+						color="#6D2080"
+					>
+					</v-autocomplete>
+				</v-flex>
+				<v-flex xs12 sm4>
+					<v-autocomplete
+						v-model="filtro2"
+						v-if="filtered"
+						:items="filterList"
+						item-text="nome"
+						item-value="value"
+						:label="filtered"
+						style="width: 80%"
+						hide-details
+						color="#6D2080"
+						>
+					</v-autocomplete>
+				</v-flex>
+				<v-flex xs12 sm4>
+					<v-btn
+					color="#6D2080"
+					dark
+					@click="[getFlags(filtered, filtro2)]"	
+					>
+						Pesquisar
+					</v-btn>	
+				</v-flex>
+			</v-row>
+			<v-row>
+				<v-flex v-for="flag in FlagList" :key="flag.index" sm4 xs12>
+					<img width="80%" height="80%" :src="flag.flag"/>
+				</v-flex>
+			</v-row>
+		</v-container>
+	</div>
 </template>
           
 <script>
 // import ArticleItem from "./ArticleItem"
-  export default {
-    name: 'Home',
-    components: {
+export default {
+	name: 'Home',
+	components: {
       // ArticleItem,
-    },
-    data: () => ({
-      Filtros: [
-        {
+	},
+	data: () => ({
+		Filtros: [
+		{
           nome: "Região",
-          value: "1"
-        },
-        {
+          value: "region"
+		},
+		{
           nome: "Capital",
-          value: "2"
-        },
-        {
+          value: "capital"
+		},
+		{
           nome: "Língua",
-          value: "3"
-        },
-        {
+          value: "lang"
+		},
+		{
           nome: "País",
-          value: "4"
-        },
-        {
+          value: "name"
+		},
+		{
           nome: "Código de ligação",
-          value: "5"
-        },
-      ],
-      linguas: [],
-      filtered: null,
-      article: [],
-      FlagList: [],
-      filterList:[],
-    }),
-    methods:{
+          value: "callingcode"
+		},
+	],
+	linguas: [],
+     filtro2: null,
+     filtered: null,
+     article: [],
+     FlagList: [],
+     filterList:[],
+	}),
+	methods:{
 
-      //Método para pegar apenas os links das bandeiras de cada país
-      getFlags(){
-        this.axios.get('https://restcountries.eu/rest/v2/all?fields=flag')
-        .then((res) => {
-          this.FlagList = res.data;
-        })
-        // this.FlagSlice = this.FlagList.slice(0, 12);
-      },
+     //Método para pegar apenas os links das bandeiras de cada país
+		getFlags(filtered, filtro2){
+			if(filtro2 != null){
+				this.axios.get(`https://restcountries.eu/rest/v2/${filtered}/${filtro2}?fields=flag`)
+				.then((res) => {
+					this.FlagList = res.data;
+				})
+			}else{
+				this.axios.get(`https://restcountries.eu/rest/v2/${filtered}?fields=flag`)
+				.then((res) => {
+					this.FlagList = res.data;
+				})
+			}
+			
+		// this.FlagSlice = this.FlagList.slice(0, 12);
+		},
 
-      async filter(){
-        switch (this.filtered) {
-          case "1":
-            this.axios.get('https://restcountries.eu/rest/v2/all?fields=region')
-            .then((res)=> {
-              this.filterList = res.data.filter(element => {
-                element.nome = element.region
-                return element;
-              });
-            })
-            break;
-          case "2":
-            this.axios.get('https://restcountries.eu/rest/v2/all?fields=capital')
-            .then((res)=>{
-              this.filterList = res.data.filter(element => {
-                element.nome = element.capital
-                return element;
-              });
-            })
-            break;
-          case "3":
-            await this.axios.get('https://restcountries.eu/rest/v2/all?fields=languages')
-            .then((res)=>{
-              this.linguas = res.data;
-            })
-            this.filterList = this.linguas.map((lingua)=>{
-              for(var i=0; i<lingua.languages.length; i++){
-                if(lingua){
-                  return {
-                    id: lingua.languages[i].iso639_1,
-                    nome: lingua.languages[i].nativeName
-                  }
-                }
-              }
-            })
-            break;
-          case "4":
-              this.axios.get('https://restcountries.eu/rest/v2/all?fields=name')
-            .then((res)=>{
-              this.filterList = res.data.filter(element => {
-                element.nome = element.name
-                return element;
-              })
-            })
-            break;
-          case "5":
-            await this.axios.get('https://restcountries.eu/rest/v2/all?fields=callingCodes')
-            .then((res)=>{
-              this.calling = res.data;
-            })
-            this.filterList = this.calling.map((call)=>{
-              for(var i=0; i<call.callingCodes.length; i++){
-                if(call){
-                  return {
-                    nome: call.callingCodes[i]
-                  }
-                }
-              }
-            })
-            break;
-          default:
-            this.axios.get('https://restcountries.eu/rest/v2/all?fields=name')
-            .then((res)=>{
-              this.filterList = res.data.filter(element => {
-                element.nome = element.name
-                return element;
-              })
-            })
-            break;
-        }
-      }
-    },
-    mounted(){
-      this.getFlags()
-    },
-  }
+		async filter(){
+			switch (this.filtered) {
+				//filtro por região
+				case "region":
+					this.axios.get('https://restcountries.eu/rest/v2/all?fields=region')
+					.then((res)=> {
+						this.filterList = res.data.filter(element => {
+							element.nome = element.region
+							element.value = element.region
+							return element;
+						});
+					})
+				break;
+				//filtro por capital
+				case "capital":
+					this.axios.get('https://restcountries.eu/rest/v2/all?fields=capital')
+					.then((res)=>{
+						this.filterList = res.data.filter(element => {
+							element.nome = element.capital
+							element.value = element.capital
+							return element;
+						});
+					})
+				break;
+				//filtro por língua
+				case "lang":
+					await this.axios.get('https://restcountries.eu/rest/v2/all?fields=languages')
+					.then((res)=>{
+						this.linguas = res.data;
+					})
+					this.filterList = this.linguas.map((lingua)=>{
+						for(var i=0; i<lingua.languages.length; i++){
+							if(lingua){
+								return {
+									nome: lingua.languages[i].nativeName,
+									value: lingua.languages[i].iso639_1
+								}
+							}
+						}
+					})
+				break;
+				//filtro por país
+				case "name":
+					this.axios.get('https://restcountries.eu/rest/v2/all?fields=name')
+					.then((res)=>{
+						this.filterList = res.data.filter(element => {
+							element.nome = element.name
+							element.value = element.name
+							return element;
+						})
+					})
+				break;
+				//filtro por codigo de ligação
+				case "callingcode":
+					await this.axios.get('https://restcountries.eu/rest/v2/all?fields=callingCodes')
+					.then((res)=>{
+						this.calling = res.data;
+					})
+					this.filterList = this.calling.map((call)=>{
+						for(var i=0; i<call.callingCodes.length; i++){
+							if(call){
+								return {
+									nome: call.callingCodes[i],
+									value: call.callingCodes[i]
+								}
+							}
+						}
+					})
+				break;
+				//default país
+				default:
+					this.axios.get('https://restcountries.eu/rest/v2/all?fields=name')
+					.then((res)=>{
+						this.filterList = res.data.filter(element => {
+							element.nome = element.name
+							return element;
+						})
+					})
+				break;
+			}
+		}
+	},
+	mounted(){
+		this.getFlags("all", null)
+	},
+}
 </script>
